4278c2fc92cd95155d53fd40d8e2c03abae61d15
Nathan Sanford <nwsanfor24@gmail.com>
2020-03-25 15:43:42 -0400

    created orm to see all, create and update burgers

--- config/orm.js
+++ config/orm.js
@@ -1,15 +1,84 @@
 const connection = require("../config/connection.js");
 
-function selectAll() {
-    
+function printQuestionMarks(num) {
+    let arr = [];
+
+    for (var i = 0; i < num; i++) {
+        arr.push("?");
+    }
+
+    return arr.toString();
 }
 
-function insertOne() {
+function objToSql(ob) {
+    let arr = [];
 
+    for (var key in ob) {
+        let value = ob[key];
+
+        if (Object.hasOwnProperty.call(ob, key)) {
+            if (typeof value === "string" && value.indexOf(" ") >= 0) {
+                value = "'" + value + "'";
+            }
+
+            arr.push(key + "=" + value);
+        }
+    }
+
+    return arr.toString();
 }
 
-function updateOne() {
+const orm = {
+    //Select all burgers
+    all: function(tableInput, cb) {
+        let queryString = "SELECT * FROM " + tableInput + ";";
+        connection.query(queryString, function(err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+    //Create new burger
+    create: function(table, cols, vals, cb) {
+        let queryString = "INSERT INTO " + table;
 
-}
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
 
+        console.log(queryString);
+
+        connection.query(queryString, vals, function(err, result) {
+            if (err) {
+                throw err;
+            }
+
+            cb(result);
+        });
+    },
+    //Update a Burger
+    update: function(table, objColVals, condition, cb) {
+        let queryString = "UPDATE " + table;
+
+        queryString += " SET ";
+        queryString += objToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
+
+        console.log(queryString);
+        connection.query(queryString, function(err, result) {
+            if (err) {
+                throw err;
+            }
+
+            cb(result);
+        });
+    }
+};
+
+//Export the ORM object
 module.exports = orm;
