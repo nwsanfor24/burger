633fa091d5046cd4c3fa799cfe460cf1384d5661
Nathan Sanford <nwsanfor24@gmail.com>
2020-03-25 15:18:36 -0400

    populated schema and seeds

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Jalapeno Cheddar Bacon Burger', false);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', false);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('BBQ Chicken Burger', false);
