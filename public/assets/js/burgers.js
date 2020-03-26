e1c91f8ada0c16cbd36f2b71b8b31094d3775504
Nathan Sanford <nwsanfor24@gmail.com>
2020-03-26 13:53:34 -0400

    finished burgers.js with button functionality

---
+++ public/assets/js/burgers.js
@@ -0,0 +1,58 @@
+$(function() {
+    $(".change-devour").on("click", function(event) {
+        let id = $(this).data("id");
+        let newDevour = $(this).data("newDevour");
+
+        let newDevourState = {
+            devoured: newDevour
+        };
+
+        //PUT request
+        $.ajax("/api/burgers/" + id, {
+            type: "PUT",
+            data: newDevourState
+        }).then(
+            function() {
+                console.log("Changed devoured to", newDevour);
+
+                location.reload();
+            }
+        );
+    });
+
+    $(".create-form").on("submit", function(event) {
+        event.preventDefault();
+
+        let newBurger = {
+            name: $("#br").val().trim(),
+            devoured: $("[name=devoured]:checked").val().trim()
+        };
+
+        //Send POST request
+        $.ajax("/api/burgers", {
+            type: "POST",
+            data: newBurger
+        }).then(
+            function() {
+                console.log("Created new burger");
+
+                location.reload();
+            }
+        );
+    });
+
+    $(".delete-burger").on("click", function(event) {
+        let id = $(this).data("id");
+
+        //Send DELETE request
+        $.ajax("/api/burgers/" + id, {
+            type: "DELETE"
+        }).then(
+            function() {
+                console.log("Deleted burger", id);
+
+                location.reload();
+            }
+        );
+    });
+});
